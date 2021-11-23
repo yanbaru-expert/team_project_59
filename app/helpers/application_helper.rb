1fe440c84c486457dd017c8acc6035cce9df1905
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-11-23 23:02:16 +0900

    ページ全体の設定を追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def max_width
+    if controller_name == "texts" && action_name == "show"
+      "mw-md"
+    # Devise 導入後にコメントアウトを解除
+    # elsif devise_controller?
+    #  "mw-sm"
+    else
+      "mw-xl"
+    end
+  end
 end
