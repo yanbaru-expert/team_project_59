351d8d772f410303bfb0d334f85610c482c11548
yutatsune <66116053+yutatsune@users.noreply.github.com>
2021-12-04 19:38:35 +0900

    seeds.rbの文字化けを修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 email = "test@example.com"
 password = "password"
-# テストユーザーが存在しないときだけ作成
 User.find_or_create_by!(email: email) do |user|
   user.password = password
-  puts "\u30E6\u30FC\u30B6\u30FC\u306E\u521D\u671F\u30C7\u30FC\u30BF\u30A4\u30F3\u30DD\u30FC\u30C8\u306B\u6210\u529F\u3057\u307E\u3057\u305F\u3002"
+  puts "The user's initial data import was successful."
 end
