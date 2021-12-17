c1a628a52ea559af801325685fd2d3de939bcfd4
teru <titlt.aiw6@gmail.com>
2021-12-17 11:02:28 +0900

    テキスト・動画教材のデータ投入処理を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,21 @@ User.find_or_create_by!(email: email) do |user|
   user.password = password
   puts "The user's initial data import was successful."
 end
+
+require "csv"
+
+CSV.foreach("db/csv_data/text_data.csv", headers: true) do |row|
+  Text.create!(
+    genre: row["genre"],
+    title: row["title"],
+    content: row["content"]
+  )
+end
+
+CSV.foreach("db/csv_data/movie_data.csv", headers: true) do |row|
+  Movie.create!(
+    genre: row["genre"],
+    title: row["title"],
+    url: row["url"]
+  )
+end
