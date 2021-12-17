6e319250fa4e5903b1d9a69eef0ecd51c10af86f
teru <titlt.aiw6@gmail.com>
2021-12-17 09:55:54 +0900

    テーブル再生成のコードを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
+# texts, movies テーブルを再生成(関連付くテーブルを含む)
+%w[texts movies].each do |table_name|
+  ActiveRecord::Base.connection.execute("TRUNCATE TABLE #{table_name} RESTART IDENTITY CASCADE")
+end
+
 email = "test@example.com"
 password = "password"
 User.find_or_create_by!(email: email) do |user|
